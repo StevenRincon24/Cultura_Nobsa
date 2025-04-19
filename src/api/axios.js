1477b7b8b24fc20eb1db8caf0417cc42a70f9853
hovar.rincon@uptc.edu.co <hovar.rincon@uptc.edu.co>
2025-04-18 19:25:05 -0500

    Refactor: URL API

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instace = axios.create({
-  baseURL: "http://localhost:3001/api",
-  //baseURL: "https://api-cultura.vercel.app/api",
+  //baseURL: "http://localhost:3001/api",
+  baseURL: "https://api-cultura.vercel.app/api",
   withCredentials: true,
 });
 
