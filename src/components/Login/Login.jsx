6d4f016996a1594836693b82e6f08f807a46db23
hovar.rincon@uptc.edu.co <hovar.rincon@uptc.edu.co>
2025-04-28 17:07:18 -0500

    Refactor: Responsive NavBar Color mobile v2

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "@/context/AuthContext";
 import { ToastContainer } from "react-toastify";
+import Hero from "@/section/Hero";
 
 const LoginForm = () => {
   const {
@@ -23,82 +24,91 @@ const LoginForm = () => {
   }, [isAuthenticated]);
 
   return (
-    <div className="font-sans ">
-      <div className="relative min-h-screen flex flex-col sm:justify-center items-center bg-gray-100">
-        <div className="relative sm:max-w-sm w-full">
-          <div className="card bg-blue-400 shadow-lg w-full h-full rounded-3xl absolute transform -rotate-6"></div>
-          <div className="card bg-red-400 shadow-lg w-full h-full rounded-3xl absolute transform rotate-6"></div>
-          <div className="relative w-full rounded-3xl px-6 py-4 bg-gray-100 shadow-md">
-            <label className="block mt-3 text-sm text-gray-700 text-center font-semibold">
-              Iniciar sesión
-            </label>
-            {loginError.map((error, i) => (
-              <div
-                className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
-                role="alert"
-                key={i}
-              >
-                <span className="block sm:inline">{error}</span>
-              </div>
-            ))}
-            <form className="mt-10" onSubmit={onSubmit}>
-              <div>
-                <input
-                  type="text"
-                  placeholder="Nombre de usuario"
-                  {...register("username", { required: true })}
-                  className="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0"
-                />
-                {errors.username && (
-                  <span className="text-red-500">El campo es requerido</span>
-                )}
-              </div>
-
-              <div className="mt-7">
-                <input
-                  type="password"
-                  placeholder="Contraseña"
-                  {...register("password", { required: true })}
-                  className="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0"
-                />
-                {errors.password && (
-                  <span className="text-red-500">
-                    La contraseña es requerida
-                  </span>
-                )}
-              </div>
-
-              <div className="mt-7 flex">
-                <label className="inline-flex items-center w-full cursor-pointer">
-                  <input
-                    type="checkbox"
-                    className="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                  />
-                  <span className="ml-2 text-sm text-gray-600">Recuérdame</span>
-                </label>
-                <div className="w-full text-right">
-                  <a
-                    className="underline text-sm text-gray-600 hover:text-gray-900"
-                    href="#"
-                  >
-                    ¿Olvidó su contraseña?
-                  </a>
-                </div>
-              </div>
-
-              <div className="mt-7">
-                <button
-                  type="submit"
-                  className="bg-blue-500 w-full py-3 rounded-xl text-white shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out transform hover:-translate-x hover:scale-105"
+    <div className="min-h-screen">
+      <Hero
+        title="Inicio de Sesión"
+        subtitle="Acceso exclusivo para usuarios autorizados"
+        imageUrl="https://images.pexels.com/photos/158732/pexels-photo-158732.jpeg"
+      />
+      <div className="font-sans ">
+        <div className="relative min-h-screen flex flex-col sm:justify-center items-center bg-gray-100">
+          <div className="relative sm:max-w-sm w-full">
+            <div className="card bg-blue-400 shadow-lg w-full h-full rounded-3xl absolute transform -rotate-6"></div>
+            <div className="card bg-red-400 shadow-lg w-full h-full rounded-3xl absolute transform rotate-6"></div>
+            <div className="relative w-full rounded-3xl px-6 py-4 bg-gray-100 shadow-md">
+              <label className="block mt-3 text-sm text-gray-700 text-center font-semibold">
+                Iniciar sesión
+              </label>
+              {loginError.map((error, i) => (
+                <div
+                  className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+                  role="alert"
+                  key={i}
                 >
-                  Iniciar sesión
-                </button>
-              </div>
-            </form>
+                  <span className="block sm:inline">{error}</span>
+                </div>
+              ))}
+              <form className="mt-10" onSubmit={onSubmit}>
+                <div>
+                  <input
+                    type="text"
+                    placeholder="Nombre de usuario"
+                    {...register("username", { required: true })}
+                    className="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0"
+                  />
+                  {errors.username && (
+                    <span className="text-red-500">El campo es requerido</span>
+                  )}
+                </div>
+
+                <div className="mt-7">
+                  <input
+                    type="password"
+                    placeholder="Contraseña"
+                    {...register("password", { required: true })}
+                    className="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0"
+                  />
+                  {errors.password && (
+                    <span className="text-red-500">
+                      La contraseña es requerida
+                    </span>
+                  )}
+                </div>
+
+                <div className="mt-7 flex">
+                  <label className="inline-flex items-center w-full cursor-pointer">
+                    <input
+                      type="checkbox"
+                      className="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                    />
+                    <span className="ml-2 text-sm text-gray-600">
+                      Recuérdame
+                    </span>
+                  </label>
+                  <div className="w-full text-right">
+                    <a
+                      className="underline text-sm text-gray-600 hover:text-gray-900"
+                      href="#"
+                    >
+                      ¿Olvidó su contraseña?
+                    </a>
+                  </div>
+                </div>
+
+                <div className="mt-7">
+                  <button
+                    type="submit"
+                    className="bg-blue-500 w-full py-3 rounded-xl text-white shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out transform hover:-translate-x hover:scale-105"
+                  >
+                    Iniciar sesión
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
+        <ToastContainer />
       </div>
-      <ToastContainer />
     </div>
   );
 };
